2f8461274e5e3300a00eed41b95523b8d3978d77
Markus Skyttner <mskyttner@gmail.com>
2016-08-31 20:48:54 +0200

    fix mix-up of 'ben' and 'scientist' user grants

--- s6init.sql
+++ s6init.sql
@@ -4,11 +4,11 @@ create user if not exists 'ben'@'%' identified by 'ben';
 grant select, insert, update, delete, lock tables, 
 	alter, create, drop, 
 	index, grant option on specify6.* 
-	to 'scientist'@'%';
+	to 'ben'@'%';
 
-grant reload on *.* to 'scientist'@'%';
+grant reload on *.* to 'ben'@'%';
 
 grant select, insert, update, delete, lock tables 
-	on specify6.* to 'ben'@'%';
+	on specify6.* to 'scientist'@'%';
 
 flush privileges;
